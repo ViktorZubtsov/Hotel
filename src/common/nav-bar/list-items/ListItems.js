ce96ad38afc451891bb76a31e95b9139f9cb7935
zubtsov_viktor@geos.local <zubtsov_viktor@smartworld.team>
2022-01-21 19:06:16 +0700

    add sass

--- src/common/nav-bar/list-items/ListItems.js
+++
@@ -1,9 +0,0 @@
-const ListItems = (props) => {
-  const {link, text} = props.value
-  return (
-      <li>
-        <a href={link}>{text}</a>
-      </li>
-  )
-}
-export default ListItems
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './app/App.css';
-import App from './app/App';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <div>
+          sdsds
+        </div>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

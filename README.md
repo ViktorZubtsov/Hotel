94fc8791ef8da36c5aa5cd9d476bf029e9afa85e
zubtsov_viktor@geos.local <zubtsov_viktor@smartworld.team>
2022-01-28 18:26:48 +0700

    refactor

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hotel
+Pet project
+
+Desing: https://drive.google.com/file/d/1Lfuqiwh-hzjxeWbpil-xBz3Daf3SULh2/view?usp=sharing
---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = ()=>  {
+  return (
+    <div className="App">
+        sdds
+    </div>
+  );
+}
+
+export default App;

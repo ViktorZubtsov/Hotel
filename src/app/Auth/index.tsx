c11fff252f0b8677bdc813aa5ff7783c837951b4
viktor <mr.viktor.1996@yandex.ru>
2022-02-06 15:07:06 +0700

    add seo

--- src/app/Auth/index.tsx
+++ src/app/Auth/index.tsx
@@ -1,13 +1,7 @@
-import HelmetCard from "../../component/HelmetCard";
 
 const Auth = () => {
   return(
       <div>
-        <HelmetCard
-            title="Auth Page"
-            description="description"
-            keywords="keywords"
-        />
         Auth
       </div>
   )

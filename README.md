051a012cc6ada4d8d9ba03f970b49239642c6dba
viktor <mr.viktor.1996@yandex.ru>
2022-02-12 21:44:48 +0700

    start

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 Pet project
 
 Desing: https://drive.google.com/file/d/1Lfuqiwh-hzjxeWbpil-xBz3Daf3SULh2/view?usp=sharing
+
+
+## :open_file_folder: Project structure
+
+```
+Hotel
+├── shared - Компоненты дизайн системы 
+├── app 
+    ├── Main  - Главная
+        ├── Main.scss
+        ├── Main.tsx
+    ├── Rooms Страница с выбором номера и фильтрами  
+        ├──       
+    ├── App.scss
+    ├── App.tsx
+├── package.json
+
+```
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,11 @@
 import './app.scss';
-import { Routes, Route } from "react-router-dom";
-import Default from "../Layouts/Default";
-import Main from "./Main";
-import Rooms from "./Rooms";
-import Auth from "./Auth";
 
 
 const App = ()=>  {
   return (
-      <Routes>
-          <Route path="/" element={<Default/>}>
-            <Route index element={<Main/>}/>
-            <Route  path="rooms/*" element={<Rooms/>} />
-            <Route  path="auth" element={<Auth/>} />
-            <Route path="*" element={<div>404</div>}/>
-          </Route>
-      </Routes>
+    <div>
+      App
+    </div>
   );
 }
 export default App;

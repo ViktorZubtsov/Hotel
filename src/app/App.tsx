fc9f4ed77e0e2d6618c5c86944525cb98398426a
viktor <mr.viktor.1996@yandex.ru>
2022-02-12 22:22:32 +0700

    lazy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,26 @@
 import './app.scss';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Default from "../Layouts/Default";
-import Main from "./Main";
-import Auth from "./Auth";
-import Rooms from "./Rooms";
-import NotFound from "./NotFound";
 
+const  Main = lazy(()=> import('./Main'));
+const  Rooms = lazy(()=> import('./Rooms'));
+const  Auth = lazy(()=> import('./Auth'));
+const  NotFound = lazy(()=> import('./NotFound'));
 
 const App = ()=>  {
   return (
-      <Routes>
-        <Route path="/" element={<Default/>}>
-          <Route index element={<Main/>}/>
-          <Route  path="rooms/*" element={<Rooms/>} />
-          <Route  path="auth" element={<Auth/>} />
-          <Route path="*" element={<NotFound/>}/>
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path="/" element={<Default/>}>
+            <Route index element={<Main/>}/>
+            <Route  path="rooms/*" element={<Rooms/>} />
+            <Route  path="auth" element={<Auth/>} />
+            <Route path="*" element={<NotFound/>}/>
+          </Route>
+        </Routes>
+      </Suspense>
+
   );
 }
 export default App;
